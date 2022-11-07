297f564ae7b3b9782b60ac19973ed6a3089a04d9
Areful <arefulnyc@gmail.com>
2022-11-08 00:21:22 +0600

    test

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
 			<main className={styles.main}>
 				<h1 className={styles.title}>
-					Welcome to <a href='https://nextjs.org'>Next.js! pages</a>
+					Welcome to <a href='https://nextjs.org'>test demo pages</a>
 				</h1>
 
 				<p className={styles.description}>
