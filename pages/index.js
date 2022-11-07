c8ab4800f9de9bc6b84241dfa3f66ca6f2c9a341
Areful <arefulnyc@gmail.com>
2022-11-08 00:23:56 +0600

    test 2

--- pages/index.js
+++ pages/index.js
@@ -16,10 +16,7 @@ export default function Home() {
 					Welcome to <a href='https://nextjs.org'>test demo pages</a>
 				</h1>
 
-				<p className={styles.description}>
-					Get started by editing{" "}
-					<code className={styles.code}>pages/index.js</code>
-				</p>
+				<p className={styles.description}>lorem ipsum</p>
 
 				<div className={styles.grid}>
 					<a href='https://nextjs.org/docs' className={styles.card}>
