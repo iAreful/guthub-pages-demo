211003d339aec174237514dbc1d3149d30f3fec5
Areful <arefulnyc@gmail.com>
2022-11-08 00:25:17 +0600

    tst

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
 
 					<a href='https://nextjs.org/learn' className={styles.card}>
 						<h2>Learn &rarr;</h2>
-						<p>Learn about Next.js in an interactive course with quizzes!</p>
+						<p>a Next.js in an interactive course with quizzes!</p>
 					</a>
 
 					<a
